25f1b362e02706c58b5431b537826103c61f50dd
FullStackPete <psliwa.b@gmail.com>
2025-01-19 23:09:39 +0100

    feat: Fix vimade integration with current config

--- lua/custom/plugins/vimade.lua
+++ lua/custom/plugins/vimade.lua
@@ -1,31 +1,24 @@
 return {
 	"tadaa/vimade",
+	enabled = true,
 	opts = {
 		recipe = { "default", { animate = true } },
 		fadelevel = 0.65,
+		enabled = false,
 		tint = {
 			fg = { rgb = { 120, 120, 120 }, intensity = 1 }, -- all text will be gray
 		},
 		blocklist = {
-			default = {
+			custom = {
 				highlights = {
-					-- Prevent ActiveTabs from highlighting.
-					"TabLineSel",
-					-- Exact highlight names are supported:
-					"WinSeparator",
-					-- Lua patterns are supported, just put the text between / symbols:
-					"/^SignColumn.*/",
-					"/^StatusLine.*/",
-					"LuaLine",
-					"/^LuaLine.*/",
+					"EndOfBuffer",
+					"/^NeoTree.*/",
+					--"/^lualine.*/", -- Uncomment this if You want to make lualine transparent
+					"/^.undotree.*/",
+					"/^undotree_highlight.*/",
+					"/^Undotree.*/",
+					"/^Outline.*/",
 				},
-				buf_opts = { buftype = { "prompt" } },
-				-- buf_name = {'name1','name2', name3'},
-				-- buf_vars = { variable = {'match1', 'match2'} },
-				-- win_opts = { option = {'match1', 'match2' } },
-				-- win_vars = { variable = {'match1', 'match2'} },
-				-- win_type = {'name1','name2', name3'},
-				-- win_config = { variable = {'match1', 'match2'} },
 			},
 		},
 	},
