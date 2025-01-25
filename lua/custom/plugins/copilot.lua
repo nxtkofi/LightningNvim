5da6d07336986d1c3ade7b0251b7e7aa76659b7f
FullStackPete <psliwa.b@gmail.com>
2025-01-25 19:26:31 +0100

    feat: update typr. Remove completion from typr files

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -4,7 +4,18 @@ return {
 	event = "InsertEnter",
 	config = function()
 		require("copilot").setup({
-
+			filetypes = {
+				yaml = false,
+				markdown = false,
+				help = false,
+				gitcommit = false,
+				gitrebase = false,
+				hgcommit = false,
+				svn = false,
+				cvs = false,
+				["."] = false,
+				typr = false,
+			},
 			suggestion = {
 				auto_trigger = true,
 			},
