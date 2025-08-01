8d5898d3a6deb0424546fe8f12194b3895184a92
nxtkofi <psliwa.b@gmail.com>
2025-08-01 20:33:04 +0200

    fix: fix obsidian daily template problem

--- lua/setup.lua
+++ lua/setup.lua
@@ -21,10 +21,10 @@ local setup = {
 	-- You can change folder names to Your liking.
 	-- Just make sure that You don't change formattings. Leave '~' and './' where they are.
 	obsidian_dirs = {
-		templates = "~/path/to/vault/templates/", -- point to a templates folder in Your zettelkasten like so: ~/myvaults/mainvault/templates/
+		templates = "templates/", -- point to a templates folder in Your zettelkasten like so: ~/myvaults/mainvault/templates/
 		dailynotes = "./daily", -- point to a daily notes folder in Your zettelkasten like so: ./daily/
 		attachments = "myAssets/", -- point to an assets folder in Your zettelkasten like so: assets/
-		generalpath = "~/path/to/vault/", --~/myvaults/mainvault/
+		generalpath = "~/path/to/vault/", -- ~/myvaults/mainvault/
 		mainnotes = "~/path/to/vault/mainnotes/", -- This direction specifies what is the main folder for Your notes. This information is used in dashboard - to pick a random note from Your main notes.
 	},
 	chat_dir = "~/path/to/vault/botchats/", -- This folder can be anywhere in the filesystem. I just happen to want to have my LLM chats inside zettelkasten, so I can reference to them anytime I want.
